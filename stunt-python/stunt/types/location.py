ab01d47d16727f5426a082f228d7a819bd315371
gabrik <gabriele.baldoni@gmail.com>
2023-04-18 09:56:46 +0200

    Fix perfect perception drive
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- stunt-python/stunt/types/location.py
+++ stunt-python/stunt/types/location.py
@@ -1,6 +1,6 @@
 from carla import Vector3D as CarlaVector3D
 from carla import Location as CarlaLocation
-
+import numpy as np
 from stunt.types import Vector2D, Vector3D
 
 import math
@@ -126,6 +126,97 @@ class Location(IdlStruct):
     def as_vector3(self):
         return Vector3D(self.x, self.y, self.z)
 
+    def l1_distance(self, other):
+        """Calculates the L1 distance between the point and another point.
+
+        Args:
+            other (:py:class:`~.Vector3D`): The other vector used to
+                calculate the L1 distance to.
+
+        Returns:
+            :obj:`float`: The L1 distance between the two points.
+        """
+        return (
+            abs(self.x - other.x)
+            + abs(self.y - other.y)
+            + abs(self.z - other.z)
+        )
+
+    def l2_distance(self, other) -> float:
+        """Calculates the L2 distance between the point and another point.
+
+        Args:
+            other (:py:class:`~.Vector3D`): The other vector used to
+                calculate the L2 distance to.
+
+        Returns:
+            :obj:`float`: The L2 distance between the two points.
+        """
+        vec = np.array([self.x - other.x, self.y - other.y, self.z - other.z])
+        return np.linalg.norm(vec)
+
+    def magnitude(self):
+        """Returns the magnitude of the 3D vector."""
+        return np.linalg.norm(self.as_numpy_array())
+
+    def as_numpy_array(self):
+        """Retrieves the 3D vector as a numpy array."""
+        return np.array([self.x, self.y, self.z])
+
+    def as_numpy_array_2D(self):
+        """Drops the 3rd dimension."""
+        return np.array([self.x, self.y])
+
+    def to_camera_view(self, extrinsic_matrix, intrinsic_matrix):
+        """Converts the given 3D vector to the view of the camera using
+        the extrinsic and the intrinsic matrix.
+
+        Args:
+            extrinsic_matrix: The extrinsic matrix of the camera.
+            intrinsic_matrix: The intrinsic matrix of the camera.
+
+        Returns:
+            :py:class:`.Vector3D`: An instance with the coordinates converted
+            to the camera view.
+        """
+        position_vector = np.array([[self.x], [self.y], [self.z], [1.0]])
+
+        # Transform the points to the camera in 3D.
+        transformed_3D_pos = np.dot(
+            np.linalg.inv(extrinsic_matrix), position_vector
+        )
+
+        # Transform the points to 2D.
+        position_2D = np.dot(intrinsic_matrix, transformed_3D_pos[:3])
+
+        # Normalize the 2D points.
+        location_2D = type(self)(
+            float(position_2D[0] / position_2D[2]),
+            float(position_2D[1] / position_2D[2]),
+            float(position_2D[2]),
+        )
+        return location_2D
+
+    def rotate(self, angle: float):
+        """Rotate the vector by a given angle.
+
+        Args:
+            angle (float): The angle to rotate the Vector by (in degrees).
+
+        Returns:
+            :py:class:`.Vector3D`: An instance with the coordinates of the
+            rotated vector.
+        """
+        x_ = (
+            math.cos(math.radians(angle)) * self.x
+            - math.sin(math.radians(angle)) * self.y
+        )
+        y_ = (
+            math.sin(math.radians(angle)) * self.x
+            - math.cos(math.radians(angle)) * self.y
+        )
+        return type(self)(x_, y_, self.z)
+
     def __repr__(self):
         return self.__str__()
 
@@ -150,7 +241,6 @@ class Location(IdlStruct):
         return type(self)(
             x=self.x - other.x, y=self.y - other.y, z=self.z - other.z
         )
-
 
     @classmethod
     def from_dict(cls, dictionary):
